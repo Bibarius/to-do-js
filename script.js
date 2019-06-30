ada1e06ce168e1eba827a8f21ff80fc45193e3e7
Bibarius <verastuk.vasil@yandex.ru>
2019-07-01 01:14:52 +0700

    added script date initialization

---
+++ script.js
@@ -0,0 +1,29 @@
+//инициализируем дату
+var now = new Date();
+var year = now.getFullYear();
+var month = now.getMonth();
+var date = now.getDate();
+var day = now.getDay();
+switch(day){
+	case 0: day = "ВОСКРЕСЕНЬЕ"; break;
+	case 1: day = "ПОНЕДЕЛЬНИК"; break;
+	case 2: day = "ВТОРНИК"; break;
+	case 3: day = "СРЕДА"; break;
+	case 4: day = "ЧЕТВЕРГ"; break;
+	case 5: day = "ПЯТНИЦА"; break;
+	case 6: day = "СУББОТА"; break;
+}
+switch(month){
+	case 0: month = "ЯНВ"; break; 
+	case 1: month = "ФЕВ"; break;
+	case 2: month = "МАР"; break;
+	case 3: month = "АПР"; break;
+	case 4: month = "МАЙ"; break;
+	case 5: month = "ИЮН"; break;
+	case 6: month = "ИЮЛ"; break;
+	case 7: month = "АВГ"; break;
+	case 8: month = "СЕН"; break;
+	case 9: month = "ОКТ"; break;
+	case 10: month = "НОЯ"; break;
+	case 11: month = "ДЕК"; break;
+}
